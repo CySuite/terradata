4d3b367c62bf751b3e19ec6bf9b9ac62ecc1ac2c
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-12-13 14:04:06 -0800

    Convert HTML to JSX and copy index.css to globals.css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,96 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (<p>Hello World</p>)
+	function dropEditor() {
+		let myLabels = document.querySelectorAll('.lbl-toggle');
+
+		Array.from(myLabels).forEach(label => {
+			label.addEventListener('keydown', e => {
+				// 32 === spacebar
+				// 13 === enter
+				if (e.which === 32 || e.which === 13) {
+					e.preventDefault();
+					label.click();
+				};
+			});
+		});
+	}
+
+	return (
+		<>
+			<div className="header">
+				<h1>Terra Data</h1>
+			</div>
+			<div className="main">
+				<div className="flex-container">
+					<div className="editor-box-1">
+						<textarea
+							className="data-input"
+							rows={4}
+							cols={50}
+							style={{ resize: "none" }}
+							value=""
+							defaultValue={""}
+						/>
+					</div>
+					<div className="editor-box-2">
+						<textarea
+							className="data-input"
+							rows={4}
+							cols={50}
+							style={{ resize: "none" }}
+							value=""
+							defaultValue={""}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="wrap-collabsible">
+				<input
+					id="collapsible"
+					className="toggle"
+					type="checkbox"
+					defaultChecked=""
+					onClick={dropEditor}
+				/>
+				<label htmlFor="collapsible" className="lbl-toggle" tabIndex={0}>
+					See code
+				</label>
+				<div className="collapsible-content">
+					<div className="content-inner">
+						<textarea
+							className="data-input"
+							rows={4}
+							cols={50}
+							style={{ resize: "none" }}
+							value=""
+							defaultValue={""}
+						/>
+					</div>
+				</div>
+			</div>
+			<footer>
+				<div className="footer">
+					<div className="row">
+						<ul className="footer-links">
+							<li>
+								<a href="#">Contact us</a>
+							</li>
+							<li>
+								<a href="#">Say Thanks!</a>
+							</li>
+							<li>
+								<a href="#">Privacy Policy</a>
+							</li>
+							<li>
+								<a href="#">Terms &amp; Conditions</a>
+							</li>
+						</ul>
+					</div>
+					<div className="row">Copyright © 2022 CySuite - All rights reserved</div>
+				</div>
+			</footer>
+		</>
+	)
 }
